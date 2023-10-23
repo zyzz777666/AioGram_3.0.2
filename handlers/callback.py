585548462db73d0af69235cf21e369ebaaf328db
I <bumbum.bug@yandex.ru>
2023-10-23 20:17:35 +0300

    development 1.0.0

--- handlers/callback.py
+++ handlers/callback.py
@@ -3,7 +3,6 @@ from aiogram.types import CallbackQuery
 from config_data.config import get_text_horoscope
 from keyboards import inline_keyboard
 from lexicon.lexicon import LEXICON_RU
-from aiogram.exceptions import TelegramBadRequest
 
 router = Router()
 day = ''
@@ -25,8 +24,9 @@ async def get_day_horoscope(callback: CallbackQuery):
 @router.callback_query(F.data == 'back')
 async def back_menu(callback: CallbackQuery):
     await callback.answer()
-    await callback.message.edit_text(text=LEXICON_RU['/start'],
-                                     reply_markup=inline_keyboard.get_time_zodiac())
+    await callback.message.answer(text=LEXICON_RU['/start'],
+                                  reply_markup=inline_keyboard.get_time_zodiac())
+    await callback.message.delete()
 
 
 @router.callback_query()
@@ -35,8 +35,8 @@ async def get_horoscope(callback: CallbackQuery):
     await callback.answer()
     zodiac = callback.data
     text = await get_text_horoscope(zodiac=zodiac, day=day)
-    try:
-        await callback.message.edit_text(text=text,
-                                         reply_markup=inline_keyboard.get_zodiac_button())
-    except TelegramBadRequest:
-        await callback.answer()
+
+    await callback.message.answer_photo(photo=LEXICON_RU[zodiac],
+                                        caption=text,
+                                        reply_markup=inline_keyboard.get_zodiac_button())
+    await callback.message.delete()
--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -3,7 +3,22 @@ LEXICON_RU: dict[str, str] = {
 
     '/help': 'Я могу отправить гороскоп на нужый вам день\неделя\месяц',
 
-    'no_echo': 'Не понимаю вас 🤯'
+    'no_echo': 'Не понимаю вас 🤯',
+    
+    'aries': 'https://hrlider.ru/posts/wp-content/uploads/2022/09/karernyj-goroskop-znaki-zodiaka-oven.jpg',
+    'taurus': 'https://w.forfun.com/fetch/69/690e774c8ee51f8a3314861102d184be.jpeg',
+    'gemini': 'https://astromagicmoon.ru/wp-content/uploads/2021/01/zodiac-sign-4374407_1920.jpg',
+    'cancer': 'https://avatars.mds.yandex.net/get-mpic/4591438/img_id4773863479411690725.jpeg/orig',
+    'leo': 'https://tvcenter.ru/wp-content/uploads/2022/11/lev-goroskop-na-mesyats-1500x1061.jpg',
+    'virgo': 'https://w.forfun.com/fetch/ad/adf61f2e4487f31f2fb7801f87c4ab33.jpeg',
+    'libra': 'https://hrlider.ru/posts/wp-content/uploads/2022/09/goroskop-znaki-zodiaka-vesy.jpg',
+    'scorpio': 'https://avatars.dzeninfra.ru/get-zen_doc/1921148'
+               '/pub_64e1ee26dafbcd6f481d0096_64e1efcec216b40f6c0c40e2/scale_1200 ',
+    'sagittarius': 'https://w.forfun.com/fetch/03/0340cceb9feff90e4e607b901b3014e9.jpeg',
+    'capricorn': 'https://w.forfun.com/fetch/44/448f3bec1a0404e51802a8f0cbf48e12.jpeg',
+    'aquarius': 'https://w.forfun.com/fetch/e7/e74dd08a2b7e7a96502edda006ec1492.jpeg',
+    'pisces': 'https://astromagicmoon.ru/wp-content/uploads/2021/03/zodiac-sign-4374416_1920.jpg',
+
 
 }
 
